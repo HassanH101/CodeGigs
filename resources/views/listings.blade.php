c0c8a28a50b955fb222a59f9de83e026f809e80f
HassanH101 <hassanhijazi456@gmail.com>
2024-11-26 16:18:07 +0200

    .

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,7 +7,9 @@
 
 @foreach  ($listings as $listing)
     <h2>
-        {{$listing['title']}}
+        <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
     </h2>
-    <p> {{$listing['description']}} </p>
+    <p>
+        {{$listing['description']}}
+    </p>
 @endforeach
