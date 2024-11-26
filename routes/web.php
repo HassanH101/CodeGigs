f4d8d2a5b228f7b4f73b2b1f3ba40912bf538d5f
HassanH101 <hassanhijazi456@gmail.com>
2024-11-26 13:58:03 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('listings', ['heading' => 'Latest Listings']);
-    
-});
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            ],
+        ],
 
+
+
+    ]);
+});
