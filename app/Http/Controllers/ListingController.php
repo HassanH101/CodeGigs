e9b6c2c9e980d5dfd08af8791e5cc8413a4c8e98
HassanH101 <hassanhijazi456@gmail.com>
2024-12-17 14:34:57 +0200

    .

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -1,26 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Listing;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
-use PhpParser\Node\Expr\List_;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     //show all listings
-    public function index() {
+    public function index()
+    {
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
     //show single listing
-    public function show(Listing $listing) {
-    return view('listing.show', [
-        'listing' => $listing
-    ]);
+    public function show(Listing $listing)
+    {
+        return view('listing.show', [
+            'listing' => $listing
+        ]);
+    }
+    //show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+        Listing::create($formFields);
+        return redirect("/");
+    }
 }
-}
-
-
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,11 @@ use App\Models\Listing;
 //All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+//Create listing
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing data 
+Route::get('/listings', [ListingController::class, 'store']);
+
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
